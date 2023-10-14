6da007898c8df31ad35ec4e232f1547bba4549b9
Matthias <83934411+mnigmann@users.noreply.github.com>
2023-10-14 15:02:42 -0400

    Added missing config.h

---
+++ config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIDTH 500
+#define HEIGHT 500
+#define COLOR_AXES 64
+#define COLOR_MAJOR_GRID 160
+#define COLOR_MINOR_GRID 200
+
+#define POINT_SIZE 3
+#define MAX_POLYGON_SIZE 4
+
+#define DT_DERIV 1e-4
+#define RADIUS_SCALE 1
+#define MAX_ARC_LENGTH 0.1
+
+#define PI 3.14159265359
+#define SCROLL_AMOUNT 1.1
+#define TICK_SIZE 125
+
+#endif
