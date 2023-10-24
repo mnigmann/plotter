1a0167067ab39a6235a56bc867b30dc99cca53e7
Matthias <83934411+mnigmann@users.noreply.github.com>
2023-10-24 09:25:36 -0400

    Removed lines.c and lines.h

--- lines.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-#ifndef LINES_H
-#define LINES_H
-
-void LineAA(uint8_t *ptr, uint64_t size_width, uint64_t size_height, uint64_t pt1x, uint64_t pt1y, uint64_t pt2x, uint64_t pt2y, const void* color );
-
-#endif
